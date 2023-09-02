f90cd89f3e21ab05e5970e1bc5f2040bb7c0736e
Shane <shane@swalker.dev>
2023-09-02 07:56:21 -0500

    prisma fix applied to web, not sure if this is the right place

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,16 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   }
 };
 
