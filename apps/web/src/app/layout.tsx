28d7cea9126841c6fc08eb4080cdb147745210f0
Shane <shane@swalker.dev>
2023-09-02 16:40:16 -0500

    bunch of style changes, probably should be in a branch/PR

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import "./globals.css";
+import Image from "next/image";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SocketProvider } from "./_components/socketProvider";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { ConnectionIndicator } from "./_components/ConnectionIndicator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +23,24 @@ export default function RootLayout({
     <ClerkProvider>
       <SocketProvider>
         <html lang="en">
-          <body className={`${inter.className} h-[calc(100vh-32px)] bg-slate-500`}>{children}</body>
+          <body
+            className={`${inter.className} h-[calc(100vh-32px)] bg-slate-200`}
+          >
+            <div className="flex flex-col w-full h-full">
+              <div className="flex bg-slate-800 h-24 items-center justify-between px-3 text-slate-100">
+                <Link href="/">
+                  <Image src="/logo.png" width={100} height={100} alt="logo" />
+                </Link>
+                <div className="flex flex-col justify-end items-center h-full">
+                  <div className="min-w-20">
+                    <UserButton />
+                  </div>
+                  <ConnectionIndicator />
+                </div>
+              </div>
+              <div className="px-3 pt-2 h-full">{children}</div>
+            </div>
+          </body>
         </html>
       </SocketProvider>
     </ClerkProvider>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,14 @@
-
 import { prisma } from "db";
-import { UserButton } from "@clerk/nextjs";
 import UserList from "./_components/users/UserList";
-import { ConnectionIndicator } from "./_components/ConnectionIndicator";
 
 export default async function Home() {
   const users = await prisma.user.findMany({});
 
   return (
-    <main className="flex min-h-screen flex-col p-24">
-      <UserButton />
-      <UserList users={users} />
-      <ConnectionIndicator />
+    <main className="flex min-h-screen flex-col p-3 h-full">
+      <div id="right-section" className="self-end w-52 h-full">
+        <UserList users={users} />
+      </div>
     </main>
   );
 }
